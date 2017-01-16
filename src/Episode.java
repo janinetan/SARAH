0dae76cf4b09ef6678ead0fd84e170f296968aff
janinetan <janine_tan14@yahoo.com>
2017-01-16 18:17:26 +0800

    Initial Commit

---
+++ src/Episode.java
@@ -0,0 +1,27 @@
+
+public class Episode {
+	private int id;
+	private int episodeGoalId;
+	private int eventId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getEpisodeGoalId() {
+		return episodeGoalId;
+	}
+	public void setEpisodeGoalId(int episodeGoalId) {
+		this.episodeGoalId = episodeGoalId;
+	}
+	public int getEventId() {
+		return eventId;
+	}
+	public void setEventId(int eventId) {
+		this.eventId = eventId;
+	}
+	
+	
+}
---
+++ src/Response.java
@@ -0,0 +1,41 @@
+
+public class Response {
+	private int id;
+	private int discourseActId;
+	private String message;
+	private int virtualAgentId;
+	private String responseType;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getDiscourseActId() {
+		return discourseActId;
+	}
+	public void setDiscourseActId(int discourseActId) {
+		this.discourseActId = discourseActId;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getVirtualAgentId() {
+		return virtualAgentId;
+	}
+	public void setVirtualAgentId(int virtualAgentId) {
+		this.virtualAgentId = virtualAgentId;
+	}
+	public String getResponseType() {
+		return responseType;
+	}
+	public void setResponseType(String responseType) {
+		this.responseType = responseType;
+	}
+	
+	
+}
