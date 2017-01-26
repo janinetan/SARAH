b1d65c98c11e5c02539992591bcb6fef3c63dc36
janinetan <janine_tan14@yahoo.com>
2017-01-26 14:10:23 +0800

    SQL File

---
+++ SARAH_KB.sql
@@ -0,0 +1,343 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sarah_kb
+-- ------------------------------------------------------
+-- Server version	5.7.9-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `action`
+--
+
+DROP TABLE IF EXISTS `action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `action` (
+  `eventId` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `objectId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `action`
+--
+
+LOCK TABLES `action` WRITE;
+/*!40000 ALTER TABLE `action` DISABLE KEYS */;
+/*!40000 ALTER TABLE `action` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `body_part`
+--
+
+DROP TABLE IF EXISTS `body_part`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `body_part` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `body_part`
+--
+
+LOCK TABLES `body_part` WRITE;
+/*!40000 ALTER TABLE `body_part` DISABLE KEYS */;
+/*!40000 ALTER TABLE `body_part` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dialogue`
+--
+
+DROP TABLE IF EXISTS `dialogue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dialogue` (
+  `eventId` int(11) NOT NULL,
+  `message` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dialogue`
+--
+
+LOCK TABLES `dialogue` WRITE;
+/*!40000 ALTER TABLE `dialogue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dialogue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `discourse_act`
+--
+
+DROP TABLE IF EXISTS `discourse_act`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `discourse_act` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `discourse_act`
+--
+
+LOCK TABLES `discourse_act` WRITE;
+/*!40000 ALTER TABLE `discourse_act` DISABLE KEYS */;
+/*!40000 ALTER TABLE `discourse_act` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `episode`
+--
+
+DROP TABLE IF EXISTS `episode`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `episode` (
+  `id` int(11) NOT NULL,
+  `episodeGoalId` int(11) NOT NULL,
+  `eventId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `episode`
+--
+
+LOCK TABLES `episode` WRITE;
+/*!40000 ALTER TABLE `episode` DISABLE KEYS */;
+/*!40000 ALTER TABLE `episode` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `episode_goal`
+--
+
+DROP TABLE IF EXISTS `episode_goal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `episode_goal` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `episode_goal`
+--
+
+LOCK TABLES `episode_goal` WRITE;
+/*!40000 ALTER TABLE `episode_goal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `episode_goal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `episode_set`
+--
+
+DROP TABLE IF EXISTS `episode_set`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `episode_set` (
+  `id` int(11) NOT NULL,
+  `sicknessId` int(11) NOT NULL,
+  `episodeId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `episode_set`
+--
+
+LOCK TABLES `episode_set` WRITE;
+/*!40000 ALTER TABLE `episode_set` DISABLE KEYS */;
+/*!40000 ALTER TABLE `episode_set` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `virtualAgentId` int(11) NOT NULL,
+  `eventType` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `object`
+--
+
+DROP TABLE IF EXISTS `object`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `object` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `imageFilePath` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `object`
+--
+
+LOCK TABLES `object` WRITE;
+/*!40000 ALTER TABLE `object` DISABLE KEYS */;
+/*!40000 ALTER TABLE `object` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `response`
+--
+
+DROP TABLE IF EXISTS `response`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `response` (
+  `id` int(11) NOT NULL,
+  `discourseActId` int(11) NOT NULL,
+  `message` varchar(200) NOT NULL,
+  `virtualAgentId` int(11) NOT NULL,
+  `responseType` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `response`
+--
+
+LOCK TABLES `response` WRITE;
+/*!40000 ALTER TABLE `response` DISABLE KEYS */;
+/*!40000 ALTER TABLE `response` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `semantic`
+--
+
+DROP TABLE IF EXISTS `semantic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `semantic` (
+  `id` int(11) NOT NULL,
+  `concept1` varchar(45) NOT NULL,
+  `concept2` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `semantic`
+--
+
+LOCK TABLES `semantic` WRITE;
+/*!40000 ALTER TABLE `semantic` DISABLE KEYS */;
+/*!40000 ALTER TABLE `semantic` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sickness`
+--
+
+DROP TABLE IF EXISTS `sickness`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sickness` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `definition` varchar(200) NOT NULL,
+  `symptoms` varchar(200) NOT NULL,
+  `causes` varchar(200) NOT NULL,
+  `treatement` varchar(200) NOT NULL,
+  `prevention` varchar(200) NOT NULL,
+  `bodyPartId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sickness`
+--
+
+LOCK TABLES `sickness` WRITE;
+/*!40000 ALTER TABLE `sickness` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sickness` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_peer`
+--
+
+DROP TABLE IF EXISTS `virtual_peer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_peer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `emotionalState` varchar(45) DEFAULT NULL,
+  `isSick` tinyint(1) DEFAULT NULL,
+  `imageFilePath` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_peer`
+--
+
+LOCK TABLES `virtual_peer` WRITE;
+/*!40000 ALTER TABLE `virtual_peer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `virtual_peer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-26 14:08:36
