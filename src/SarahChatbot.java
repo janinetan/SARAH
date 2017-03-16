26cc5912d54331aed7d26aea21ec0cca16cf36d8
regalabianca <regalabianca@gmail.com>
2017-03-16 15:05:01 +0800

    SARAH CHATBOT
    aiml shizzz

---
+++ src/SarahChatbot.java
@@ -0,0 +1,25 @@
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+
+public class SarahChatbot {
+
+	public static void main (String[] args){
+		String s = "What do you think! user ? Liam.should rest";
+		s = getVerdict(s);
+		System.out.println("Verdict: "+s);
+	}
+	
+	public static String getVerdict (String input){
+		String path = "C:/Users/Bianca/Downloads/sarah-bot";
+		Bot bot = new Bot("sarah-bot", path);
+		Chat chat = new Chat(bot);
+		
+		input = input.replaceAll("\\?", " 999 ");
+		input = input.replaceAll("[!\\.]", " ");
+		
+		input = chat.multisentenceRespond(input);
+		
+		String[] output = input.split("V>");
+		return output[1];	
+	}
+}
